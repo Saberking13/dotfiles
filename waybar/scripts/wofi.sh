4c4d79b9d77de3334ca3f063382eed61f52555ff
lemniskett <lemniskett@outlook.com>
2020-07-31 21:04:53 +0700

    Use pidof instead of tmp cache for wofi.

--- waybar/scripts/wofi.sh
+++ waybar/scripts/wofi.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-CACHE_PATH=/tmp/swaytmp/wofiopened
-
-if [ ! -e $CACHE_PATH ]; then
-  wofi --show drun --hide-scroll --xoffset=3 --yoffset=3 --width=600 \
-    --height=400 &
-  touch $CACHE_PATH
-else
-  killall wofi && rm $CACHE_PATH
-fi
+pidof wofi && killall wofi || wofi --show drun --hide-scroll --xoffset=3 \
+  --yoffset=3 --width=600 --height=400
