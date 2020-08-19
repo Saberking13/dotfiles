9d4a3491d0324b8a06025718a8fa92044969f281
lemniskett <lemniskett@outlook.com>
2020-08-19 18:49:35 +0700

    Waybar changes.

--- desktop/fish/config.fish
+++ desktop/fish/config.fish
@@ -12,12 +12,13 @@ mkdir -p /tmp/swaytmp
 chmod 0700 /tmp/swaytmp
 set -x XDG_RUNTIME_DIR /tmp/swaytmp
 set -x XDG_CONFIG_HOME $HOME/.config
-set -x PATH $PATH /home/lemniskett/.cargo/bin /home/lemniskett/.local/share/bin /home/lemniskett/.local/bin/ .
+set -x PATH $PATH /home/lemniskett/.cargo/bin /home/lemniskett/.local/share/bin /home/lemniskett/.local/bin/ /home/lemniskett/Documents/Projects/flutter/bin
 set -x XDG_CACHE_HOME $HOME/.cache
 set -x XDG_DATA_HOME $HOME/.local/share
 set -x XDG_CURRENT_DESKTOP Unity
 set -x MOZ_ENABLE_WAYLAND 1
 set -x QT_QPA_PLATFORMTHEME qt5ct
+set -x _JAVA_AWT_WM_NONREPARENTING 1
 # set -x QT_STYLE_OVERRIDE kvantum
 if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
     # dbus-launch --exit-with-session sway
--- desktop/waybar/scripts/wofi.sh
+++ desktop/waybar/scripts/wofi.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 pidof wofi && killall wofi || wofi --show drun --hide-scroll --xoffset=3 \
-  --yoffset=3 --width=600 --height=400
+  --yoffset=3 --width=320 --height=360 --prompt "Search applications..." \
+  --insensitive
