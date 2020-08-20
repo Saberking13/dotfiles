22ed44f25a5bc55614ca7a0f81c28532d87c878e
lemniskett <lemniskett@outlook.com>
2020-08-20 11:33:06 +0700

    Some changes.

--- desktop/sway/scripts/covid-id.sh
+++ desktop/sway/scripts/covid-id.sh
@@ -3,4 +3,4 @@
 country="ID"
 confirmed=$(curl -L https://covid19.mathdro.id/api/countries/$country | jq ".confirmed" | jq ".value")
 
-echo $confirmed > ~/.cache/covid
+[[ -z $confirmed ]] || echo $confirmed > ~/.cache/covid
