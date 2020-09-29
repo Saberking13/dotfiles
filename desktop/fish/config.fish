1a0a077274a439c821da5037f3deb8e25a9377d4
lemniskett <lemniskett@outlook.com>
2020-09-29 20:11:04 +0700

    Too many changes, i don't remember.

--- desktop/fish/config.fish
+++ desktop/fish/config.fish
@@ -12,16 +12,15 @@ mkdir -p /tmp/swaytmp
 chmod 0700 /tmp/swaytmp
 set -x XDG_RUNTIME_DIR /tmp/swaytmp
 set -x XDG_CONFIG_HOME $HOME/.config
-set -x PATH $PATH /home/lemniskett/.cargo/bin /home/lemniskett/.local/share/bin /home/lemniskett/.local/bin/ /home/lemniskett/Documents/Projects/flutter/bin
+set -x PATH $PATH /home/lemniskett/.cargo/bin /home/lemniskett/.local/share/bin /home/lemniskett/.local/bin/ /sbin /usr/sbin
 set -x XDG_CACHE_HOME $HOME/.cache
 set -x XDG_DATA_HOME $HOME/.local/share
 set -x XDG_CURRENT_DESKTOP Unity
-set -x QT_QPA_PLATFORM wayland
+#set -x QT_QPA_PLATFORM wayland
 set -x MOZ_ENABLE_WAYLAND 1
 set -x QT_QPA_PLATFORMTHEME qt5ct
 set -x _JAVA_AWT_WM_NONREPARENTING 1
 set -x SIXEL_BGCOLOR '#2e3440'
-set -x STEAM_RUNTIME 0
 # set -x QT_STYLE_OVERRIDE kvantum
 if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
     # dbus-launch --exit-with-session sway
