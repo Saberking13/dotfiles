dc8731fd912df64d7bce4b741418042182b9e71d
lemniskett <lemniskett@outlook.com>
2021-01-09 23:50:33 +0700

    Some changes.

--- desktop/sway/autostart.sh
+++ desktop/sway/autostart.sh
@@ -14,11 +14,7 @@ sleep 2
 mako &
 ~/.config/sway/scripts/battery_notify.sh &
 archbox megasync &
+mpd &
 
 # Auto starts after 15 seconds upon login
 sleep 13
-
-while true; do
-  ~/.config/sway/scripts/covid-id.sh
-  sleep 600
-done
