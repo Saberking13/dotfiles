cd9ac7f32e2ef7be4ef0144b96842665449e795b
lemniskett <lemniskett@outlook.com>
2021-02-04 09:49:31 +0700

    Some changes I don't remember.

--- desktop/sway/autostart.sh
+++ desktop/sway/autostart.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 ~/.config/sway/scripts/turn_off_screen.sh &
+swaybg -i ~/.config/sway/images/wallpaper.png &
 xhost si:localuser:root &
 foot --server &
-mkfifo /tmp/swaytmp/wobpipe &
-sleep 1
-kill $(pidof pulseaudio)
-pulseaudio &
+#kill $(pidof pulseaudio)
+#pulseaudio &
 waybar &
 ibus-daemon &
 # Auto starts after 2 seconds upon login
 sleep 2
+archbox --mount-runtime-only
 mako &
 ~/.config/sway/scripts/battery_notify.sh &
 archbox megasync &
